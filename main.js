f6623f6f005229b5d3530df7f9d081a901901404
Izawa Ryosuke <ethan@mba.local>
2021-03-19 02:48:59 +0900

    first

---
+++ main.js
@@ -0,0 +1,26 @@
+const inputElem = document.getElementById("example"); // input要素
+const currentValueElem = document.getElementById("current-value"); // 埋め込む先のspan要素
+
+// 現在の値をspanに埋め込む関数
+const setCurrentValue = (val) => {
+  currentValueElem.innerText = val;
+  console.log(val);
+};
+
+// inputイベント時に値をセットする関数
+const rangeOnChange = (e) => {
+  setCurrentValue(e.target.value);
+};
+
+window.onload = () => {
+  inputElem.addEventListener("input", rangeOnChange); // スライダー変化時にイベントを発火
+  setCurrentValue(inputElem.value); // ページ読み込み時に値をセット
+};
+
+//controllerの背景色をスライダーで変える
+const controller = document.getElementById("controller");
+
+const changeColor = () => {
+  console.log("pushed btn");
+  controller.style.backgroundColor = "rgba(128,128,255,1)";
+};
